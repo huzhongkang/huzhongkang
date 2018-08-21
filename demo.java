073c7f15293edbf3d3fc8da449df8111b26511b0
huzhongkang <zhongkang.hu.git@email.com>
2018-08-21 16:52:24 +0800

    修改

--- demo.java
+++ demo.java
@@ -1,9 +1,13 @@
 package huzhongkang;
 
+import java.util.ArrayList;
+
 public class demo {
 
     public static void main(String args[]){
        System.out.println("aaaa");
+
+        ArrayList<Object> arrayList = new ArrayList<Object>();
 
     }
 }
