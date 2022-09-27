53b576e2f7a0f156ea83b68312f06b39a7256231
sp0182 <Hzk624105>
2022-09-27 15:19:09 +0800

    da

--- demo.java
+++ demo.java
@@ -1,17 +1,1 @@
-package huzhongkang;
-
-import java.util.ArrayList;
-
-public class demo {
-
-    public static void main(String args[]){
-       System.out.println("aaaa");
-
-        ArrayList<Object> arrayList = new ArrayList<Object>();
-
-        arrayList.add("huzhongkang");
-        arrayList.add("nizhenshuai");
-        arrayList.add("nizhenshuai");
-		
-    }
-}
+o o cccc
